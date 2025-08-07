80ac57ab80a64d97498ff1c7937d947b395b9b78
Akash Patel <akash906129@gmail.com>
2025-08-07 18:59:10 +0530

    impl db fn

--- store/src/models/user.rs
+++ store/src/models/user.rs
@@ -1,8 +1,54 @@
 use crate::store::Store;
+use diesel::prelude::*;
+use uuid::{Uuid, uuid};
 
+#[derive(Queryable, Selectable, Insertable)]
+#[diesel(table_name = crate::schema::user)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+
+pub struct User {
+    id: String,
+    username: String,
+    password: String,
+}
 
 impl Store {
-    pub fn crete_user(&self) {
-        print!("create user");
+    pub fn sign_in(
+        &mut self,
+        username: String,
+        password: String,
+    ) -> Result<String, diesel::result::Error> {
+        let user_id = Uuid::new_v4();
+        let u = User {
+            username,
+            password,
+            id: user_id.to_string(),
+        };
+
+        diesel::insert_into(crate::schema::user::table)
+            .values(&u)
+            .returning(User::as_returning())
+            .get_result(&mut self.conn)?;
+
+        Ok(user_id.to_string())
+    }
+
+    pub fn sign_up(
+        &mut self,
+        username_input: String,
+        password_input: String,
+    ) -> Result<bool, diesel::result::Error> {
+        use crate::schema::user::dsl::*;
+
+        let user_result = user
+            .filter(username.eq(username_input))
+            .select(User::as_select())
+            .first(&mut self.conn)?;
+
+        if user_result.password != password_input {
+            return Ok(false);
+        }
+
+        Ok(true)
     }
 }
--- store/src/models/website.rs
+++ store/src/models/website.rs
@@ -1,11 +1,32 @@
 use crate::store::Store;
+use chrono::Utc;
+use diesel::prelude::*;
+use uuid::{Uuid, uuid};
 
+#[derive(Queryable, Selectable, Insertable)]
+#[diesel(table_name = crate::schema::website)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+
+pub struct Website {
+    id: String,
+    url: String,
+    time_added: chrono::NaiveDateTime,
+    user_id: String
+}
 
 impl Store {
     
-    pub fn create_website(&self, url: String) -> String {
-        
-        return String::from("ksfnskbfkd");
+    pub fn create_website(&mut self, user_id:String, url: String) -> String {
+        let id =Uuid::new_v4();
+        let website=Website{
+            user_id,
+            url,
+            id:id.to_string(),
+            time_added:Utc::now().naive_local()
+
+        };
+
+        let website = diesel::insert_into(c)
     }
     pub fn get_website(&self, url: String) -> String {
        
